2bed8a69b49b0c2516f5df279e55e4348489b6fb
onlineformsins-art <onlineformsins@gmail.com>
2026-04-21 14:55:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -133,3 +133,19 @@ function App() {
 }
 
 export default App;
+// src/App.tsx or src/components/Books.tsx
+
+const base = import.meta.env.BASE_URL;
+
+function Books() {
+  return (
+    <div>
+      <h1>My Books</h1>
+      <img src={`${base}books/image1.jpg`} alt="Book 1" />
+      <img src={`${base}books/image2.jpg`} alt="Book 2" />
+      {/* Add more books here */}
+    </div>
+  );
+}
+
+export default Books;
