47a8c0f8dc727b7262b77cefebbb9b753e98625d
onlineformsins-art <onlineformsins@gmail.com>
2026-04-21 16:52:42 +0530

    Update Books.tsx

--- src/src/components/Books.tsx
+++ src/src/components/Books.tsx
@@ -4,8 +4,22 @@ function Books() {
   return (
     <div>
       <h1>My Books</h1>
-      <img src="/mishkathbooks/books/image1.jpg" alt="Book 1" />
-      <img src="/mishkathbooks/books/image2.jpg" alt="Book 2" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_001.png" alt="Book 1" />   
+      <img src="/mishkathbooks/blob/main/app/public/books/page_002.png" alt="Book 2" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_003.png" alt="Book 3" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_004.png" alt="Book 4" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_005.png" alt="Book 5" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_006.png" alt="Book 6" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_007.png" alt="Book 7" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_008.png" alt="Book 8" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_009.png" alt="Book 9" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_010.png" alt="Book 10" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_011.png" alt="Book 11" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_012.png" alt="Book 12" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_013.png" alt="Book 13" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_014.png" alt="Book 14" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_015.png" alt="Book 15" />
+      <img src="/mishkathbooks/blob/main/app/public/books/page_016.png" alt="Book 16" />
     </div>
   );
 }
