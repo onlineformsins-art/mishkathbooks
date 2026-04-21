477298aebaeeca044f01af9b1a1e690258c5134c
onlineformsins-art <onlineformsins@gmail.com>
2026-04-21 14:49:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   base: '/mishkathbooks/',
